89bacf6be71a551bb912aa2a908002c3bc845d69
Sandipan0110 <sandipanseal11@gmail.com>
2021-12-31 00:08:38 +0530

    Refactor : Final Updated Add Label API with TDD

--- app/service/label.js
+++ app/service/label.js
@@ -2,7 +2,7 @@ const labelModel = require("../models/label.js");
 
 class LabelService {
     addLabel = (label, callback) => {
-        labelModel.addLabel(label, (error, data) => {
+        labelModel.addlabelById(label, (error, data) => {
             if (error) {
               return callback(error, null);
             }
