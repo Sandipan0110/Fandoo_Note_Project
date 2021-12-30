c092f6df540b7300a27cde2133653adf4ba80378
Sandipan0110 <sandipanseal11@gmail.com>
2021-12-31 02:50:41 +0530

    Added : Update Invalid token Checking in GetLabelByid API with TDD

--- test/label.js
+++ test/label.js
@@ -303,4 +303,19 @@ describe("Get Label by Id", () => {
         return done();
       });
   });
+  it.only("when call getLabelById with invalid token , should return appropriate response from controller", (done) => {
+    const token = labelData.notes.inValidToken;
+    chai
+      .request(server)
+      .get("/getLabel")
+      .set({ authorization: token })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(400);
+        return done();
+      });
+  });
 });
